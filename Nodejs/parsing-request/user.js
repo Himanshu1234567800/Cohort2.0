e80af2a803241f5215c56713c968688a658e989f
vashixtha <142379027+Himanshu1234567800@users.noreply.github.com>
2025-11-06 18:37:19 +0530

    Parsing-request

---
+++ Nodejs/parsing-request/user.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Coding</title></head>");
+    res.write("<body><h1>Enter Your Details:</h1>");
+    res.write('<form action="/submit-details" method="POST">');
+    res.write(
+      '<input type="text" name="username" placeholder="Enter your name"><br>'
+    );
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male" />');
+    res.write('<label for="female">Female</label>');
+    res.write(
+      '<input type="radio" id="female" name="gender" value="female" />'
+    );
+    res.write('<br><input type="submit" value="Submit">');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  } else if (
+    req.url.toLowerCase() === "/submit-details" &&
+    req.method == "POST"
+  ) {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const FullBody = Buffer.concat(body).toString();
+      const params = new URLSearchParams(FullBody);
+      const bodyObject = Object.fromEntries(params);
+      console.log(bodyObject);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Parsing Request</title></head>");
+    res.write("<body><h1>Like / Share / Subscribe</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
